61e3a0e5dd2983947726b631e3568e4509c3ace7
Marcel Zwart <marcelzwart92@gmail.com>
2025-11-21 15:00:47 +0100

    Implement a REPL

--- src/main.c
+++ src/main.c
@@ -6,13 +6,16 @@ int main(int argc, char *argv[])
     // Flush after every printf
     setbuf(stdout, NULL);
 
-    // TODO: Uncomment the code below to pass the first stage
-    printf("$ ");
+    while(true)
+    {
+        // TODO: Uncomment the code below to pass the first stage
+        printf("$ ");
 
-    char input[100];
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = 0;
-    printf("%s: command not found\n", input);
+        char input[100];
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+        printf("%s: command not found\n", input);
+    }
 
     return 0;
 }
